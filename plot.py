e20f272582d8e921dc479321dafeb626dde8ca30
fmalrs <68164585+fmalrs@users.noreply.github.com>
2023-04-30 13:47:07 -0700

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# get input data
+
+df = pd.read_csv('data.csv')
+df=df.astype(float)
+
+ax = df.plot(x='Time', y=['o2', 'ph'])
+ax.set_ylabel("Concentration (PPM)")
+plt.title("Current TSAR Activity")
+plt.savefig('graph.png')
+plt.show()
