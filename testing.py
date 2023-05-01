200f64464c07016d4c41e077270a8b8bb486b64f
Alex Hillman <70306920+alexhillman@users.noreply.github.com>
2023-04-30 23:43:41 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -39,6 +39,24 @@ def button_callback():
         
         
 
+        
+def get_devices():
+    device = AtlasI2C()
+    device_address_list = device.list_i2c_devices()
+    device_list = []
+    
+    for i in device_address_list:
+        device.set_i2c_address(i)
+        response = device.query("I")
+        try:
+            moduletype = response.split(",")[1] 
+            response = device.query("name,?").split(",")[1]
+        except IndexError:
+            print(">> WARNING: device at I2C address " + str(i) + " has not been identified as an EZO device, and will not be queried") 
+            continue
+        device_list.append(AtlasI2C(address = i, moduletype = moduletype, name = response))
+    return device_list 
+
 # main program loop
 def main():
     global dataFile
