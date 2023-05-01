0117465c3931e28b3eef94d289f433a178212a39
Alex Hillman <70306920+alexhillman@users.noreply.github.com>
2023-05-01 05:08:26 -0600

    Create commit.sh

---
+++ commit.sh
@@ -0,0 +1,13 @@
+git commit -m "Experiment Data Upload"
+
+git remote add origin /home/pi/Desktop/TSAR-main
+
+git remote -v
+
+git push -u origin master
+
+git add $1
+
+git commit -m ”Experiment Data Upload”
+
+git push
