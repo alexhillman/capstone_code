808beddd4be8f97b73c560056cf93a332e49635d
Alex Hillman <70306920+alexhillman@users.noreply.github.com>
2023-05-01 05:46:29 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -342,7 +342,9 @@ def main():
                 if(numReads == 5):
                     numReads = 0
                     
-                    subprocess.call("git add " + dirName + "data.csv")
+                    subprocess.call("cd /home/pi/Desktop/TSAR-main")
+                    subprocess.call("git add " + "experiments/experiment_" + time.strftime("%Y-%m-%d_%H-%M", time.gmtime(experimentTime)) + "/data.csv")
+                    subprocess.call("git add " + "graph.png")
                     subprocess.call("git commit -m \"Data Upload\"")
                     subprocess.call("git push https://TSAR-23042-1:" + githubToken + "@github.com/TSAR-23042-1/TSAR-main.git")
                     
