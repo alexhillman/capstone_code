9752315bccc6fc4d186624915cc69015fe438f1c
fmalrs <68164585+fmalrs@users.noreply.github.com>
2023-04-30 21:14:28 -0700

    Update photoScript.py

--- photoScript.py
+++ photoScript.py
@@ -3,14 +3,21 @@ import requests
 # Import the base64 module for encoding a file to base64
 import base64
 
-githubAPIURL = "https://api.github.com/repos/fmalrs/23042prototype/contents/graph.png"
+
+f=open('access.txt')
+lines=f.readlines()
+
+
+#githubAPIURL = "https://api.github.com/repos/fmalrs/23042prototype/contents/graph.png"
+githubAPIURL = lines[5]
+
 # Replace "bracketcounters" with your username, replace "test-repo" with your repository name and replace "new-image.png" with the filename you want to upload from local to GitHub.
 
 # change access.txt to update github access file
-with open('access.txt', 'r') as file:
-    githubToken = file.read()
+#with open('access.txt', 'r') as file:
+#    githubToken = file.read()
     
-
+githubToken = lines[1]
 
 
 with open("graph.png", "rb") as f:
